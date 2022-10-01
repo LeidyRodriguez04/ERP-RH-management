d45f336570c1232f4fef650ed448197fd7c31f6f
ronin047 <99992115+ronin047@users.noreply.github.com>
2022-10-01 16:46:44 -0500

    reponsive contacto ok

--- ERPangular/src/app/components/inicio/inicio.component.ts
+++ ERPangular/src/app/components/inicio/inicio.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+    selector: 'app-inicio',
+    templateUrl: './inicio.component.html',
+    styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+    constructor() { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
