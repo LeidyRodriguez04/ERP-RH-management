d289f8740eceb37fe7be866b7e72be57a8ecaec0
ronin047 <99992115+ronin047@users.noreply.github.com>
2022-09-30 19:43:13 -0500

    subida primera parte contacto

--- ERPangular/src/app/app-routing.module.ts
+++ ERPangular/src/app/app-routing.module.ts
@@ -4,13 +4,13 @@ import { RouterModule, Routes } from '@angular/router';
 //components
 import { ContactoComponent } from './components/contacto/contacto.component';
 import { IngresoComponent } from './components/ingreso/ingreso.component';
-import { InicioComponent } from './components/inicio/inicio.component'
+import { InicioComponent } from './components/inicio/inicio.component';
 
 
 const routes: Routes = [
     {path:'', component: InicioComponent},
     {path:'ingreso', component: IngresoComponent},
-    {path:'crear-mensage', component: ContactoComponent}
+    {path:'contacto', component: ContactoComponent}
 
 ];
 
