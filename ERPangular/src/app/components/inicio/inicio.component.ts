0f1db4e32d936c4af8e295189dff16384872b0af
LeidyRodriguez04 <lrodr267@ibero.edu.co>
2022-10-05 18:38:31 -0500

    inicio terminado

--- ERPangular/src/app/components/inicio/inicio.component.ts
+++ ERPangular/src/app/components/inicio/inicio.component.ts
@@ -19,15 +19,12 @@ export class InicioComponent implements OnInit {
     responsive:{
         0:{
             items:1,
-            nav:true
         },
         700:{
             items:2,
-            nav:false
         },
         1000:{
             items:3,
-            nav:true,
             loop:false
         }
     }
