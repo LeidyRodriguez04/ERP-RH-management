75d00bd226025a992084f82e4c52492be824c44a
ronin047 <99992115+ronin047@users.noreply.github.com>
2022-10-10 18:59:45 -0500

    Create Read.....del CRUD

--- ERPexpress/controllers/contactoController.js
+++ ERPexpress/controllers/contactoController.js
@@ -1,17 +1,60 @@
-
+const Contacto = require("../models/ContactoModelo");
 // creation des conecteurs pour les controllers 
-exports.obtenerContactos = (req,res)=>{
+exports.obtenerContactos = async (req,res)=>{
     console.log('esta dando todos los contactos de la BD ')
+    try {
+        const contactos = await Contacto.find()
+        res.json(contactos)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('hay un problema ... comuniquese con el administrador') // response au serveur -- respuesta al servidor
+    }
 }
-exports.crearContacto = (req,res)=>{
-    console.log('esta creando todos los contactos de la BD ')
+exports.crearContacto = async (req,res)=>{
+    console.log('esta creando todos los contactos de la BD test ')
+    try {
+        let contacto;
+        contacto = new Contacto(req.body)
+        await contacto.save()
+        res.send(contacto)
+    } catch (error) {
+        console.log(error)
+        res.status(500).send('hay un problema ... comuniquese con el administrador')
+    }
 }
-exports.actualizarContacto = (req,res)=>{
+exports.actualizarContacto = async (req,res)=>{
     console.log('esta actualizando todos los contactos de la BD ')
+    // try {
+    //     let contacto;
+    //     contacto = new Producto(req.body)
+    //     await contacto.save()
+    //     res.send(contacto)
+    // } catch (error) {
+    //     console.log(error)
+    //     res.status(500).send('hay un problema ... comuniquese con el administrador')
+    // }
 }
-exports.borrarContacto = (req,res)=>{
+exports.borrarContacto = async (req,res)=>{
     console.log('esta borrando todos los contactos de la BD ')
+    // try {
+    //     let contacto;
+    //     contacto = new Producto(req.body)
+    //     await contacto.save()
+    //     res.send(contacto)
+    // } catch (error) {
+    //     console.log(error)
+    //     res.status(500).send('hay un problema ... comuniquese con el administrador')
+    // }
 }
-exports.obtenerContactoEspecifico = (req,res)=>{
-    console.log('esta dando un contacto especifico de la BD ')
+exports.obtenerContactoEspecifico = async (req,res)=>{
+    console.log('esta dando un contacto especifico de la BD  ')
+    // try {
+    //     let contacto;
+    //     contacto = new Producto(req.body)
+    //     await contacto.save()
+    //     res.send(contacto)
+    // } catch (error) {
+    //     console.log(error)
+    //     res.status(500).send('hay un problema ... comuniquese con el administrador')
+    // }
 }
